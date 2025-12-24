8869e5be12e1ee8e13ae9ab233925ffd17fd5c79
makssem13 <maks20sem@gmail.com>
2025-12-24 18:22:33 +0200

    Fast fix

--- ialloc.c
+++ ialloc.c
@@ -11,6 +11,9 @@ void** AllocList = NULL; //  pointer to the array of allocated memories
 int    AllocListSize = 0; //     size of AllocList
 int    AllocListCur = 0; //     current amount of allocated memory chunks
 
+
+#include "ialloc.h"
+
 void AL_Add(void* pointer, void (*ErrorHandler)()) {
 
 
