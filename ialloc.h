c335b5e33279667faa31660d0cbfa2f6072cefc8
makssem13 <maks20sem@gmail.com>
2026-05-29 18:32:04 +0300

    a fix of void -> bool in AL_Add declaration

--- ialloc.h
+++ ialloc.h
@@ -7,7 +7,7 @@ extern void** AllocList; //  pointer to the array of allocated memories
 extern int    AllocListSize; //     size of AllocList
 extern int    AllocListCur; //     current amount of allocated memory chunks
 
-void AL_Add(void* pointer, void (*ErrorHandler)());
+bool AL_Add(void* pointer, void (*ErrorHandler)());
 void* imalloc(size_t size, void (*ErrorHandler)());
 void* icalloc(size_t lengh, size_t size, void (*ErrorHandler)());
 void* irealloc(void* pointer, size_t size, void (*ErrorHandler)());
